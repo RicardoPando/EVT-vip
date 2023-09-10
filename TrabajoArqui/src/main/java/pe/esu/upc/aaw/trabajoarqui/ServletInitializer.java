2db56c592101ce0a37f9b22e52333a7b648ea6ef
RicardoPando <85509984+RicardoPando@users.noreply.github.com>
2023-09-10 13:54:28 -0500

    CreacionProyecto

---
+++ TrabajoArqui/src/main/java/pe/esu/upc/aaw/trabajoarqui/ServletInitializer.java
@@ -0,0 +1,13 @@
+package pe.esu.upc.aaw.trabajoarqui;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TrabajoArquiApplication.class);
+    }
+
+}
---
+++ TrabajoArqui/src/test/java/pe/esu/upc/aaw/trabajoarqui/TrabajoArquiApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.esu.upc.aaw.trabajoarqui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajoArquiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
